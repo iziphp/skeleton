30e8082e781aa8e9357ba36e45f2410e4aaf2074
Vusal Orujov <or.vusal@gmail.com>
2023-06-18 13:21:23 +0400

    Add HttpFactoryServiceProvider

--- config/providers.php
+++ config/providers.php
@@ -1,9 +1,14 @@
 <?php
 
 use Shared\Infrastructure\Providers\CacheServiceProvider;
+use Shared\Infrastructure\Providers\HttpFactoryServiceProvider;
 use Shared\Infrastructure\Providers\HttpServiceProvider;
 
 return [
+    /** Define implementations for cache interfaces (PSR-6, PSR-16)  */
     CacheServiceProvider::class,
+
+    /** Define implementations for HTTP factories (PSR-17) */
+    HttpFactoryServiceProvider::class,
     HttpServiceProvider::class
 ];
