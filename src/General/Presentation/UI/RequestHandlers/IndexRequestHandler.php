312aebef24a3504106daca9eda61fe3ccc442693
Vusal Orujov <or.vusal@gmail.com>
2023-07-01 19:25:30 +0400

    Rename Common module to General

--- src/General/Presentation/UI/RequestHandlers/IndexRequestHandler.php
+++ src/General/Presentation/UI/RequestHandlers/IndexRequestHandler.php
@@ -1,29 +1,27 @@
 <?php
 
-namespace Common\Presentation\UI\RequestHandlers;
+namespace General\Presentation\UI\RequestHandlers;
 
 use Easy\Http\Message\RequestMethod;
 use Easy\Router\Attributes\Route;
-use Easy\Router\Priority;
-use Laminas\Diactoros\Exception\InvalidArgumentException;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Server\RequestHandlerInterface;
-use Shared\Presentation\HTTP\Response\Response;
+use Shared\Presentation\HTTP\Response\JsonResponse;
 
 /** @package Common\Presentation\UI\RequestHandlers */
-#[Route(path: '*', method: RequestMethod::GET, priority: Priority::LOW)]
-class NotFoundRequestHandler implements RequestHandlerInterface
+#[Route(path: '/', method: RequestMethod::GET)]
+class IndexRequestHandler implements RequestHandlerInterface
 {
     /**
      * @param ServerRequestInterface $request 
      * @return ResponseInterface 
-     * @throws InvalidArgumentException 
      */
     public function handle(
         ServerRequestInterface $request
     ): ResponseInterface {
-        return (new Response())
-            ->withStatus(404);
+        return new JsonResponse([
+            'message' => 'Hello World!'
+        ]);
     }
 }
--- src/General/Presentation/UI/RequestHandlers/NotFoundRequestHandler.php
+++ src/General/Presentation/UI/RequestHandlers/NotFoundRequestHandler.php
@@ -1,27 +1,29 @@
 <?php
 
-namespace Common\Presentation\UI\RequestHandlers;
+namespace General\Presentation\UI\RequestHandlers;
 
 use Easy\Http\Message\RequestMethod;
 use Easy\Router\Attributes\Route;
+use Easy\Router\Priority;
+use Laminas\Diactoros\Exception\InvalidArgumentException;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Server\RequestHandlerInterface;
-use Shared\Presentation\HTTP\Response\JsonResponse;
+use Shared\Presentation\HTTP\Response\Response;
 
 /** @package Common\Presentation\UI\RequestHandlers */
-#[Route(path: '/', method: RequestMethod::GET)]
-class IndexRequestHandler implements RequestHandlerInterface
+#[Route(path: '*', method: RequestMethod::GET, priority: Priority::LOW)]
+class NotFoundRequestHandler implements RequestHandlerInterface
 {
     /**
      * @param ServerRequestInterface $request 
      * @return ResponseInterface 
+     * @throws InvalidArgumentException 
      */
     public function handle(
         ServerRequestInterface $request
     ): ResponseInterface {
-        return new JsonResponse([
-            'message' => 'Hello World!'
-        ]);
+        return (new Response())
+            ->withStatus(404);
     }
 }
