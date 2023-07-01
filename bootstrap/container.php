a97001e244ffdfbe1205c6ad4087bfbc4873ee89
Vusal Orujov <or.vusal@gmail.com>
2023-07-01 13:27:21 +0400

    Add ConfigResolver

--- bootstrap/container.php
+++ bootstrap/container.php
@@ -1,8 +1,9 @@
 <?php
 
-use Adbar\Dot;
 use Dotenv\Dotenv;
 use Easy\Container\Container;
+use Shared\Infrastructure\Config\Config;
+use Shared\Infrastructure\Config\ConfigResolver;
 
 $rootDir = dirname(__DIR__);
 
@@ -10,8 +11,7 @@ $rootDir = dirname(__DIR__);
 $dotenv = Dotenv::createImmutable($rootDir);
 
 // Load configuration.
-$config = new Dot();
-$config->set('databases', []);
+$config = new Config();
 $config->set('dirs', [
     'root' => $rootDir,
     'cache' => $rootDir . '/var/cache',
@@ -22,17 +22,11 @@ $config->set('enable_caching', env('ENVIRONMENT', 'dev') == 'prod');
 
 // Setup container.
 $container = new Container();
+$container->pushResolver(new ConfigResolver($config));
+
 $container->set('bootstrappers', require 'config/bootstrappers.php');
 $container->set('commands', require 'config/commands.php');
 $container->set('migrations', require 'config/migrations.php');
 $container->set('providers', require 'config/providers.php');
-
-// Load config values to the container. Prefix them with 'config.'.
-// Use dot notation to access nested values.
-// Example: $container->get('config.databases.default');
-
-foreach ($config->flatten(prepend: 'config.') as $abstract => $concrete) {
-    $container->set($abstract, $concrete);
-}
 
 return $container;
---
+++ src/Shared/Infrastructure/Config/Config.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Shared\Infrastructure\Config;
+
+use Adbar\Dot;
+
+class Config extends Dot
+{
+}
